c2e807fb77ab8e44227014e4752d0b7bf98742cd
aaaugusto <augustocesarmacielima2015@gmail.com>
2026-09-23 12:38:40 -0300

    Refatorando código para mudar de array simples para array circular
    Updated the insertAtRank and removeAtRank methods to handle insertion and removal at the beginning of the array correctly. Adjusted exception messages for clarity.

--- VETOR/VetorArray.java
+++ VETOR/VetorArray.java
@@ -21,7 +21,7 @@ public class VetorArray {
 
     public Object elemAtRank(Integer rank){
         if (rank < 0 || rank >= size()){
-            throw new VetorExcecoes("Rank fora do limite!");
+            throw new VetorExcecoes("Rank fora do limiteee!");
         }
         int indice = indiceFisico(rank);
         return this.array[indice];
@@ -29,7 +29,7 @@ public class VetorArray {
 
     public Object replaceAtRank(Integer rank, Object object){
         if (rank < 0 || rank >= size()){
-            throw new VetorExcecoes("Rank fora do limite!");
+            throw new VetorExcecoes("Rank fora do limiteee!");
         }
         int indice = indiceFisico(rank);
         Object remover = this.array[indice];
@@ -38,7 +38,14 @@ public class VetorArray {
     }
 
     public void insertAtRank(Integer rank, Object object){
-        if (rank >= 0 && rank <= size()){
+        if (rank == 0){
+            this.inicio_vetor = (this.inicio_vetor - 1 + this.capacidade) % this.capacidade;
+            this.array[this.inicio_vetor] = object;
+            this.tamanho++;
+
+        }
+
+        else if (rank >= 0 && rank <= size()){
             int indice_rank = indiceFisico(rank);
             for(int i = this.tamanho; i >= rank; i--){
                 indice_rank = indiceFisico(i);
@@ -48,20 +55,32 @@ public class VetorArray {
             this.array[indice_rank] = object;
             this.tamanho++;
         }
-        else { throw new VetorExcecoes("Rank fora do limite!"); }
+
+        else { throw new VetorExcecoes("Rank fora do limiteee!"); }
     }
 
     public Object removeAtRank(Integer rank){
         if (rank < 0 || rank >= size()){
-            throw new VetorExcecoes("Rank fora do limite!");
+            throw new VetorExcecoes("Rank fora do limiteee!");
         }
+
+        if (rank == 0){
+            Object remover = this.array[this.inicio_vetor];
+            this.array[this.inicio_vetor] = null;
+            this.inicio_vetor = (this.inicio_vetor + 1) % this.capacidade;
+            this.tamanho--;
+            return remover;
+        }
+
         int indice_rank = indiceFisico(rank);
         Object remover = this.array[indice_rank];
+
         for(int i = rank; i < this.tamanho - 1; i++){
             indice_rank = indiceFisico(i);
             int indice_vetor = indiceFisico(i+1);
             this.array[indice_rank] = this.array[indice_vetor];
         }
+
         int ultimo = indiceFisico(tamanho - 1);
         this.array[ultimo] = null;
         this.tamanho--;
